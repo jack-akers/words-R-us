643479e0a5303d7fcef0ffd7164f6a88e41c76b7
Anthony-D99 <anthonydisaf@yahoo.com>
2023-05-10 20:52:38 -0400

    test

--- assests/script.js
+++ assests/script.js
@@ -1,9 +1,10 @@
 var searchInput = $('#search-input')
 var searchButton = $('#search-button')
 var searchHistory = $('#search-history')
-var word = $('#searched-word')
+var word = $(searchInput).val()
 var def = $('definition')
 var synonym = $('#synonym')
+ 
 var requestDictionaryAPI=`https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=b09d1310-a453-424a-9da2-e911b084efce`
 
 

f8995f5243f8239893dedf56da5852debe8fa580
Anthony-D99 <anthonydisaf@yahoo.com>
2023-05-11 20:03:36 -0400

    pulled all information needed from dictionary api and applied the information to the webpage on search

--- assests/script.js
+++ assests/script.js
@@ -2,21 +2,32 @@ var searchInput = $('#search-input')
 var searchButton = $('#search-button')
 var searchHistory = $('#search-history')
 var word = $(searchInput).val()
-var def = $('definition')
+var def = $('#definition')
 var synonym = $('#synonym')
+var searchedWord = $('#searched-word')
+var partOfSpeach = $('#part-of-speach')
  
 
 
 function getApi(requestUrl) {
-    var requestUrl=`https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=b09d1310-a453-424a-9da2-e911b084efce`
+  word = $(searchInput).val()
+    var requestUrl=`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=b09d1310-a453-424a-9da2-e911b084efce`
 
     fetch(requestUrl)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
-       
-       console.log(data)
+        console.log(data)
+        searchedWord.text(data[0].hwi.hw)
+        partOfSpeach.text(data[0].fl)
+        
+        
+        for (i = 0; i < data[0].shortdef.length; i++) {
+          console.log(data[0].shortdef[i])
+          def.append(data[0].shortdef[i] + '; ')
+        }
+        
         
       })
   
@@ -27,7 +38,6 @@ function getApi(requestUrl) {
 
 
 
-searchButton.on('click', getApi)
 
 function getSynonymsApi(requestUrlSynonyms) {
   var requestUrlSynonyms =`https://www.stands4.com/services/v2/syno.php?uid=1001&tokenid=tk324324&word=consistent&format=json`
@@ -39,7 +49,7 @@ function getSynonymsApi(requestUrlSynonyms) {
      console.log(data)
     })
 };
-searchButton.on('click', getSynonymsApi)
+searchButton.on('click', getSynonymsApi, getApi)
 
 
 
